c0fef9394319f22fdbf8da08a96fb44ec04aed7d
Jamol <exampla@gmail.com>
2023-04-28 18:55:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// const input = [8, 6, 4, [3,2], 1,5];
+
+
+
+
+// const output = input.flat()
+// console.log(output);
+
+
+// const output1 = output.sort()
+
+// console.log(output1);
+// const output6 = output1.reverse();
+
+// const output3 = output1.filter((n) => {
+//     return n % 2 === 0;
+// })
+
+// const output5 = output1.filter((n) => {
+//     return n % 2 ;
+// })
+
+
+// const output2 = output1.reduce((output3,output5) => {
+//     return output3*output5; 
+// })
+
+
+
+// console.log(output2);
+
+
+
+// const a = 10 ;
+// const b =20; 
+// const temp;
+
+
+// temp = a;
+// a = b;
+// b = temp;
+
+
+// console.log(temp);
+// console.log(' a ning javobi',a);
+// console.log(' b ning javobi',b);
+
+
+
+// let a = 20;
+// let b = 10;
+
+// //create a temporary variable
+// let temp;
+
+// //swap variables
+// temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a);
+// console.log(b);
+
+// const bir = [1,1,1,2,2,3,3,3];
+
+// const ikki = [...new Set(bir)]
+
+// // console.log(ikki);
+
+
+// const first = [1,2,3];
+// const second = [4,5,6];
+
+// const uch = [...first, ...second];
+// console.log(uch);
+
+const a = 11;
+const b = 2;
+
+console.log(a%b);
---
+++ index.js
@@ -0,0 +1,55 @@
+// const firstName = [1,2,3,4,5,6,7];
+
+
+// const big = firstName.flat((n) => {
+//     return n ;
+
+// })
+// console.log(big);
+
+
+
+//hammasnio bir biriga kopaytirib beradi 
+
+// const big = firstName.reduce((A,B) =>{
+//     return A * B ;
+// })
+// console.log(big);
+
+
+
+
+// let output = firstName.filter((n) => {
+//     return n % 2 === 0;
+   
+// })
+// console.log(output);
+
+
+//spread operator / concat 
+
+
+
+
+
+
+
+
+
+
+//massiv method 
+
+// firstName.pop();
+// console.log(firstName);
+
+
+// firstName.push(7);
+// console.log(firstName);
+
+// firstName.shift(1)
+// console.log(firstName)
+
+// firstName.unshift(1);
+
+// console.log(firstName);
+
